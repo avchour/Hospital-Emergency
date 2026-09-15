8e8215d06d69fccc8a3c49c04b5b2f42cb96730c
Chunghour ING <chunghour561@gmail.com>
2026-09-15 00:27:14 -0400

    linkedlist data structure apply on queue concept

---
+++ src/include/PriorityQueue.h
@@ -0,0 +1,25 @@
+#ifndef PRIORITYQUEUE_H
+#define PRIORITYQUEUE_H
+
+#include "../data/Patient.h"
+
+class PriorityQueue
+{
+private:
+    struct QueueNode
+    {
+        Patient *patient;
+        QueueNode *next;
+    };
+    QueueNode *head = nullptr;
+    QueueNode *tail = nullptr;
+    int count = 0;
+
+public:
+    void addPatient(Patient *patient);
+    void treatNextPatient();
+    void displayQueue();
+    bool checkEmpty();
+};
+
+#endif
