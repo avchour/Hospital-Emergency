216065f3abacf4b7534a2f4fb7eda2f355a6035c
Chunghour ING <chunghour561@gmail.com>
2026-09-15 23:08:27 -0400

    checking treatement priority function structure

--- src/data/PriorityQueue.cpp
+++ src/data/PriorityQueue.cpp
@@ -1,1 +1,55 @@
 #include "../include/PriorityQueue.h"
+
+#include <iostream>
+
+PriorityQueue::PriorityQueue() {};
+
+PriorityQueue::~PriorityQueue()
+{
+    QueueNode *current = head;
+
+    while (current != nullptr)
+    {
+        QueueNode *temp = current;
+        current = current->next;
+        delete temp;
+    }
+};
+
+bool PriorityQueue::checkEmpty() //"Is the emergency treatment queue currently empty?"
+{
+    return head == nullptr;
+};
+
+void PriorityQueue::enqueuePatient(Patient *patient)
+{
+    if (patient == nullptr)
+        return;
+    QueueNode *queueNode = new QueueNode{patient, nullptr};
+    QueueNode *cur = head;
+    QueueNode *prev = nullptr;
+    if (checkEmpty()) // correct case1
+    {
+        head = queueNode;
+        tail = queueNode;
+    }
+    else
+    {
+        while (cur != nullptr)
+        {
+
+            if (cur->patient->priority > patient->priority)
+            {
+
+            }
+            else if (cur->patient->priority < patient->priority)
+            {
+            }
+            else
+            {
+            }
+        }
+
+        count++;
+    };
+};
